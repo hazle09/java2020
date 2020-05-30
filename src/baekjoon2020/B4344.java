b5b8d8d8c5e0426a532d255e1db42c8e90416066
hazle09 <joohyenim@gmail.com>
2020-05-30 19:38:42 +0900

    평균은 넘겠지

--- src/baekjoon2020/B4344.java
+++ src/baekjoon2020/B4344.java
@@ -13,9 +13,8 @@ public class B4344 {
 		for(int i=0;i<testcase;i++) {//중첩for문 사용함
 			int students=sc.nextInt();
 			int[] arr=new int[students];
-			double sum=0;
-			double avg=0;
-			double number=0;//평균넘는 학생 수
+			double sum=0,avg=0,number=0;//평균 넘는 학생 수
+	
 			for(int j=0;j<students;j++) {//첫번째 for문은 학생수,각 학생 점수, 평균 계산
 				arr[j]=sc.nextInt();//이거 j를 i라고 써놨다가 한시간동안 헤멤 ㅠㅠㅠ 실수 no..........
 				sum+=arr[j];
@@ -26,14 +25,15 @@ public class B4344 {
 				if(arr[k]>avg) {
 					number++;
 				}
+				
 			}
 
 			double result=(number/students)*100; //평균 넘는 학생수의 퍼센트 비율을 배열에 저장
 			percent[i]=result;
 
 		}
-		for(int i=0;i<testcase;i++) {
-			System.out.println(String.format("%.3f", percent[i])+"%"); //string.format으로 소수점 3째에서 반올림, 출력
+		for(double i:percent) {
+			System.out.println(String.format("%.3f", i)+"%"); //string.format으로 소수점 3째에서 반올림, 출력
 		}
 		sc.close();
 	}

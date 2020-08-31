21d10a19cda9f53e5faea771b93cc1c9cfd4ebd1
hazle09 <joohyenim@gmail.com>
2020-09-01 03:53:57 +0900

    반복문

--- src/Algorithm/C1_q11.java
+++ src/Algorithm/C1_q11.java
@@ -10,7 +10,7 @@ public class C1_q11 {
 		int n=sc.nextInt();
 		int number=n;
 		int count=1;
-	//자릿수를 do-while문을 이용해 제한하기
+//자릿수를 do-while문을 이용해 제한하기
 //			do{
 //			n=sc.nextInt();
 //			}while(n<10 || n>99); while(!(n<10 || n>99)); 드모르간의 법칙 ㅋ 

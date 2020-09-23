0234a3a0839b471f77651a0b28bf97e65a9d4aa5
hazle09 <joohyenim@gmail.com>
2020-09-24 00:04:00 +0900

    배열 원소찾기

---
+++ src/Algorithm/C3_q3.java
@@ -0,0 +1,39 @@
+package Algorithm;
+
+import java.util.Scanner;
+
+public class C3_q3 {
+	static int searchIdx(int[] a,int n,int key,int[] idx) {
+		int count=0;
+		for(int i=0;i<n;i++) {
+			if(a[i]==key)
+				idx[count++]=i;
+	
+		}	return count;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("요솟수: ");
+		int num=sc.nextInt();
+		int[] x=new int[num];
+		int[] y=new int[num];
+		
+		for(int i=0;i<num;i++) {
+			System.out.print("x["+i+"]:");
+			x[i]=sc.nextInt();
+		}
+		System.out.print("찾는값:");
+		int ky=sc.nextInt();
+		
+		int count=searchIdx(x,num,ky,y);
+		
+		if(count==0)
+			System.out.println("값 없삼");
+		else
+			for(int i=0;i<count;i++)
+				System.out.println("그 값은 "+"x["+y[i]+"]에 있습니다.");
+		System.out.println(count);
+	}
+
+}
